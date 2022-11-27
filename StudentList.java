e35a00ecf3520208223677fa67fc6b790cc186a1
neasher <neasherahmed75@gmail.com>
2022-11-27 10:05:14 +0600

    Fix the terminate early if the number of arguments passed into it is wrong

--- StudentList.java
+++ StudentList.java
@@ -5,6 +5,11 @@ import java.util.*;
 public class StudentList {
 	public static void main(String[] args) {
 
+		if (args == null || args[0].length() != 1) {
+			System.out.println("Please provide a, r, ?, +, or c argument");
+			return;
+		}
+
 		// Check arguments
 		if (args[0].equals("a")) {
 
@@ -93,7 +98,9 @@ public class StudentList {
 
 			System.out.println("Data Loaded.");
 
-		} else if (args[0].contains("c")) {
+		}
+
+		else if (args[0].contains("c")) {
 
 			System.out.println("Loading data ...");
 
